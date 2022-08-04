7a48b2506c75839ea66346146fc3d99e7c18e481
Rihden <nedhirboudeli@gmail.com>
2022-08-04 17:56:54 +0100

    finished backend

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const promMid = require("express-prometheus-middleware")
+
+
+
+const app = express();
+app.use(morgan("dev"))
+
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+
+    /**
+     * Uncomenting the `authenticate` callback will make the `metricsPath` route
+     * require authentication. This authentication callback can make a simple
+     * basic auth test, or even query a remote server to validate access.
+     * To access /metrics you could do:
+     * curl -X GET user:password@localhost:9091/metrics
+     */
+    authenticate: req => req.headers.authorization === 'mysecrettoken',
+}));
+
+
+app.listen("3000");
+
+app.get("/time", (req, res) => {
+    console.log(req.headers.authorization)
+    res.json({
+        epoch: Math.floor(Date.now() / 1000)
+    })
+})
